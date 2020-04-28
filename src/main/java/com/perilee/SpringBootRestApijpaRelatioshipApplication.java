3a23e0bb2dedf1e8a42186b0a6d6514faa72f302
PericlesKrH <perrykarach@gmail.com>
2020-04-28 19:22:29 +0300

    Merge origin/master into branch_Pericles
    Conflicts:
    	pom.xml
    	src/main/java/com/example/ContainerTerminal/controllers/UserController.java
    	src/main/webapp/WEB-INF/jsp/adminTable.jsp
    	src/main/webapp/WEB-INF/jsp/c_table.jsp

---
+++ src/main/java/com/perilee/SpringBootRestApijpaRelatioshipApplication.java
@@ -0,0 +1,13 @@
+package com.perilee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApijpaRelatioshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApijpaRelatioshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perilee/SpringBootRestApijpaRelatioshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perilee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApijpaRelatioshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
